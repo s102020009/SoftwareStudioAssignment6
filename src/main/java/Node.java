bb2f26bda0baafbe8e8bd7d3287f0ec13bfcd890
103062211 <amanotaiga3665@gmail.com>
2016-05-01 16:40:58 +0800

    node

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -24,12 +24,26 @@ public class Node extends Component implements DetectMouse{
 	//display node
 	@Override
 	public void display(){
+		super.getParent().noStroke();
+		super.getParent().fill(PApplet.unhex(this.colour), 200);
+		if(this.mouseOver()){
+			super.getParent().ellipse(this.currentX, this.currentY, this.radius+5, this.radius+5);
+		}else{
+			super.getParent().ellipse(this.currentX, this.currentY, this.radius, this.radius);
+		}
 	}
 	
 	//detect mouse hovering
 	@Override
 	public boolean mouseOver(){
-		return false;
+		float disX = super.getParent().mouseX - this.currentX;
+		float disY = super.getParent().mouseY - this.currentY;
+		
+		if(PApplet.sqrt(PApplet.sq(disX) + PApplet.sq(disY)) < this.radius){
+			return true;
+		}else{
+			return false;
+		}
 	}
 	
 	//add targets
@@ -44,10 +58,13 @@ public class Node extends Component implements DetectMouse{
 	
 	//set the coordinate
 	public void setPosition(float x, float y) {
+		Ani.to(this, 1, "currentX", x);
+		Ani.to(this, 1, "currentY", y);
 	}
 	
 	//reset the coordinate
 	public void resetPosition(){
+		this.setPosition(super.getX(), super.getY());
 	}
 	
 	//return the current X coordinate
