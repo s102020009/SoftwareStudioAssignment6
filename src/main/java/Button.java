2916b3e3fe7c49c50ad067ff1c204bef05158d45
103062211 <amanotaiga3665@gmail.com>
2016-05-01 10:28:56 +0800

    button

--- src/main/java/Button.java
+++ src/main/java/Button.java
@@ -13,12 +13,31 @@ public class Button extends Component implements DetectMouse{
 	
 	//display button
 	@Override
-	public void display(){		
+	public void display(){
+		if(this.mouseOver() && !super.getParent().mousePressed){
+			super.getParent().fill(185, 228, 199);
+		}else if(this.mouseOver() && super.getParent().mousePressed){
+			super.getParent().fill(0, 158, 253);
+		}else{
+			super.getParent().fill(173, 204, 180);
+		}
+		super.getParent().noStroke();
+		super.getParent().rect(super.getX(), super.getY(), this.width, this.height);
+		super.getParent().fill(255);
+		super.getParent().textSize(20);
+		super.getParent().text(super.getLabel(), super.getX()+this.width/2, super.getY()+this.height/2);
 	}
 	
 	//detect mouse hovering
 	@Override
 	public boolean mouseOver(){		
-		return false;				
+		if(super.getParent().mouseX >= super.getX() 
+		&& super.getParent().mouseX <= super.getX()+this.width
+		&& super.getParent().mouseY >= super.getY() 
+		&& super.getParent().mouseY <= super.getY()+this.height){
+			return true;
+		}else{
+			return false;
+		}				
 	}
 }
